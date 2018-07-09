4ec7b55392207b39943fde550864a3e924e09699
Matthew Kelly <mkonabike@gmail.com>
2018-07-09 09:03:25 -0500

    employees printing

--- employees.py
+++ employees.py
@@ -1,31 +1,8 @@
-# class Person:
-#   def __init__(self, name, age):
-#     self.name = name
-#     self.age = age
-
-# p1 = Person("John", 36)
-
-# print(p1.name)
-# print(p1.age)
-
-# class Person:
-#   def __init__(mysillyobject, name, age):
-#     mysillyobject.name = name
-#     mysillyobject.age = age
-
-#   def myfunc(abc):
-#     print("Hello my name is " + abc.name)
-
-# p1 = Person("John", 36)
-# p1.myfunc()
-
-class Company(object):
-    def __init__(self, company_name, date_founded, employee_1, employee_2, employee_3):
+class Company():
+    def __init__(self, company_name, date_founded, employees):
         self.company_name = company_name
         self.date_founded = date_founded
-        self.employee_1 = employee_1
-        self.employee_2 = employee_2
-        self.employee_3 = employee_3
+        self.employees = employees
 
     def get_company_name(self):
         return self.company_name
@@ -34,20 +11,34 @@ class Company(object):
         return self.date_founded
 
     def get_employees(self):
-        return self.employee_1, self.employee_2, self.employee_3
+      for x in self.employees:
+         print(x.first_name, x.last_name, x.hire_date)
 
-walmart = Company("Wal-Mart", "1971", "Matt Kelly", "David Paul", "John Who")
+class Employee():
+    def __init__(self, first_name, last_name, hire_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.hire_date = hire_date
+# make function to put employees in appropriate company
+# def company_employees ():
 
-print(walmart.get_company_name(),
-walmart.get_date_founded(),
-walmart.get_employees())
+matt = Employee("Matt", "Kelly", "July 1, 2018")
+greg = Employee("Greg", "Smelly", "July 2, 2018")
+walmart_employees = list()
+walmart_employees.append(matt)
+walmart_employees.append(greg)
 
-walmart.get_company_name()
-walmart.get_date_founded()
+walmart = Company("Wal-Mart", "1971", walmart_employees)
+
+# print(walmart.get_company_name(),
+# walmart.get_date_founded())
+
+for x in walmart_employees:
+  print(x.first_name, x.last_name, x.hire_date)
+
 walmart.get_employees()
 
-walmart.employee_3 = "Kate Wang"
-print(walmart.employee_3)
+
 
 
 
